ef6101c204fac497864a9bbc2f45b2edff6c277e
annemaijogi-sketch <annemai.jogi@tktk.ee>
2025-12-17 00:24:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,23 @@ const recipeRouter = require('./routes/recipes.routes');
 const ingredientRouter = require('./routes/ingredients.routes');
 const fullRecipesRouter = require('./routes/fullRecipes.routes');
 const randomRouter = require('./routes/randomRecipe.routes');
+
 const app = express();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   next();
+});
+
+/* TEST ROUTE – VÄGA OLULINE */
+app.get('/', (req, res) => {
+  res.send('API is running');
 });
 
 app.use('/ingredients', ingredientRouter);
@@ -16,6 +27,8 @@ app.use('/recipes', recipeRouter);
 app.use('/fullRecipes', fullRecipesRouter);
 app.use('/random', randomRouter);
 
-app.listen(3000, () => {
-    console.log('Server is running on Port 3000.');
+/* ⬇️ RENDER PORT */
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Server running on port', PORT);
 });
