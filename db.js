fe331246ead6b4ff1bffcf8a1a0c68a79c04b9cb
annemaijogi-sketch <annemai.jogi@tktk.ee>
2025-12-17 00:49:32 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,13 @@
 const dotenv = require('dotenv');
 dotenv.config();
+
 const { Pool } = require('pg');
 
-const itemsPool = new Pool ({
-    connectionString: process.env.DBConnectionString,
-    ssl: {
-        rejectUnauthorized: false
-    }
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-
-
-module.exports = itemsPool;
+module.exports = pool;
